f8cb271dc2577643172023879c56225c240c4a73
CarefreeLife <csm4903@naver.com>
2024-08-07 14:54:43 +0900

    24.08.06 FullCalendar 라이브러리 적용 및 Mock Data 생성

---
+++ cms-front/src/components/Calendar/CalendarMiniBoardItem/CalendarMiniBoardItem.tsx
@@ -0,0 +1,156 @@
+import {ChangeEvent, useEffect, useRef, useState} from "react";
+import {useBoardStore} from "../../../stores";
+import {useNavigate} from "react-router-dom";
+import {useCookies} from "react-cookie";
+import {MAIN_PATH} from "../../../constants";
+import './style.css'
+
+export default function CalendarMiniBoard() {
+    // state: 제목 영역 요소 참조 상태
+    const titleRef = useRef<HTMLTextAreaElement | null>(null);
+
+    // state: 본문 영역 요소 참조 상태
+    const contentRef = useRef<HTMLTextAreaElement | null>(null);
+
+    // state: 이미지 입력 요소 참조 상태
+    const imageInputRef = useRef<HTMLInputElement | null>(null);
+
+    // state: 게시물 상태 (전역: 페이지 상단 헤더의 '로그인' 버튼에서 하단 내용의 상태를 알아야 함.)
+    const {title, setTitle} = useBoardStore();
+    const {content, setContent} = useBoardStore();
+    const {boardImageFileList, setBoardImageFileList} = useBoardStore();
+    const {resetBoard} = useBoardStore();
+
+    // state: 게시물 이미지 미리보기 URL 상태
+    const [imageUrls, setImageUrls] = useState<string[]>([]);
+
+    // function: 네비게이트 함수
+    const navigate = useNavigate();
+
+    // state: 쿠키 상태
+    const [cookies, setCookies] = useCookies();
+
+    // event handler: 제목 변경 이벤트 처리
+    const onTitleChangeHandler = (event: ChangeEvent<HTMLTextAreaElement>) => {
+        const {value} = event.target;
+        setTitle(value);
+        if(!titleRef.current) return;
+        titleRef.current.style.height = 'auto';
+        titleRef.current.style.height = `${titleRef.current.scrollHeight}px`;
+    };
+
+    // event handler: 내용 변경 이벤트 처리
+    const onContentChangeHandler = (event: ChangeEvent<HTMLTextAreaElement>) => {
+        const {value} = event.target;
+        setContent(value);
+        if(!contentRef.current) return;
+        contentRef.current.style.height = 'auto';
+        contentRef.current.style.height = `${contentRef.current.scrollHeight}px`;
+    };
+
+    // event handler: 이미지 변경 이벤트 처리
+    const onImageChangeHandler = (event:ChangeEvent<HTMLInputElement>) => {
+        if(!event.target.files || !event.target.files.length) return;
+
+        // 미리보기 용 이미지 추출
+        const file = event.target.files[0];
+        const imageUrl = URL.createObjectURL(file);
+        const newImageUrls = imageUrls.map(item => item);
+        newImageUrls.push(imageUrl);
+        setImageUrls(newImageUrls);
+
+        // 업로드 용 이미지
+        const newBoardImageFileList = boardImageFileList.map(item => item);
+        newBoardImageFileList.push(file);
+        setBoardImageFileList(newBoardImageFileList);
+
+        if(!imageInputRef.current) return;
+        imageInputRef.current.value = '';
+    };
+
+    // event handler: 이미지 업로드 버튼 클릭 이벤트 처리
+    const onImageUploadButtonClickHandler = () => {
+        if (!imageInputRef.current) return;
+        imageInputRef.current.click();
+    };
+
+    // event handler: 이미지 제거 버튼 클릭 이벤트 처리
+    const onImageCloseButtonClickHandler = (deleteIndex: number) => {
+        if(!imageInputRef.current) return;
+        imageInputRef.current.value = '';
+
+        const newImageUrls =
+            // 삭제 버튼이 눌린 이미지를 제외한 나머지 이미지만 남겨둠.
+            imageUrls.filter((url, index) =>
+                index != deleteIndex
+            );
+        setImageUrls(newImageUrls);
+
+        const newBoardImageFileList =
+            // 삭제 버튼이 눌린 이미지를 제외한 나머지 이미지만 남겨둠.
+            boardImageFileList.filter((file, index) =>
+                index !== deleteIndex
+            );
+        setBoardImageFileList(newBoardImageFileList);
+    };
+
+    // effect: 마운트 시 실행할 함수
+    useEffect(() => {
+        const accessToken = cookies.accessToken;
+        if(!accessToken) {
+            navigate(MAIN_PATH());
+            return;
+        }
+        resetBoard();
+    }, []);
+
+    //          render: 게시물 작성 화면 렌더정          //
+    return (
+        <div id='mini-board-write-wrapper'>
+            <div className='mini-board-write-container'>
+                <div className='mini-board-write-box'>
+                    <div className='mini-board-write-title-box'>
+                        <textarea ref={titleRef}
+                                  className='mini-board-write-title-textarea'
+                                  rows={1}
+                                  placeholder='제목을 작성해주세요.'
+                                  value={title}
+                                  onChange={onTitleChangeHandler}
+                        />
+                    </div>
+                    <div className='divider'></div>
+                    <div className='mini-board-write-content-box'>
+                        <textarea ref={contentRef}
+                                  className='mini-board-write-content-textarea'
+                                  placeholder='본문을 작성해주세요.'
+                                  value={content}
+                                  onChange={onContentChangeHandler}
+                        />
+                        <div className='icon-button' onClick={onImageUploadButtonClickHandler}>
+                            <div className='icon image-box-light-icon'></div>
+                        </div>
+
+                        {/*이미지만 올릴 수 있도록 지정*/}
+                        <input ref={imageInputRef}
+                               type='file'
+                               accept='image/*'
+                               style={{display: 'none'}}
+                               onChange={onImageChangeHandler}
+                        />
+                    </div>
+                    <div className='mini-board-write-images-box'>
+                        {imageUrls.map((imageUrl, index) =>
+                            <div className='mini-board-write-image-box'>
+                                <img className='mini-board-write-image'
+                                     src={imageUrl}/>
+                                <div className='icon-button image-close' onClick={() => onImageCloseButtonClickHandler(index)}>
+                                    <div className='icon close-icon'></div>
+                                </div>
+                            </div>
+                        )}
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
--- cms-front/src/components/Calendar/index.tsx
+++ cms-front/src/components/Calendar/index.tsx
@@ -1,33 +1,110 @@
-import FullCalendar from '@fullcalendar/react'
-import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
+import React, { useState } from 'react';
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
 import interactionPlugin from '@fullcalendar/interaction';
 import timeGridPlugin from '@fullcalendar/timegrid';
 import listPlugin from '@fullcalendar/list';
 import CalendarItem from '../../types/interface/calendar-item.interface';
+import { EventClickArg } from '@fullcalendar/core';
+import Modal from 'react-modal';
+import CalendarMiniBoard from "./CalendarMiniBoardItem/CalendarMiniBoardItem";
+
+import './style.css'
+
 
 interface Props {
     calenderItemList: CalendarItem[];
 }
 
-export default function Calendar({calenderItemList}: Props) {
+Modal.setAppElement('#root');
 
-    // event handler: 일정 내 이벤트 클릭 시 이벤트 내 속성으로 가지고 있는 게시물 url 로 이동.
-    const handleEventClick = (info: any) => {
+export default function Calendar({ calenderItemList }: Props) {
+    const [modalIsOpen, setModalIsOpen] = useState(false);
+    const [selectedDate, setSelectedDate] = useState<{ start: string; end: string }>({ start: '', end: '' });
+
+    const handleModalSubmit = () => {
+
+    };
+
+    // event handler: 캘린더 내 날짜 선택/드래그 선택 시 업무 추가 모달 오픈
+    const handleSelectDate = (selectInfo: any) => {
+        setSelectedDate({ start: selectInfo.startStr, end: selectInfo.endStr });
+        setModalIsOpen(true);
+    };
+
+    // event handler: 캘린더 내 이벤트 클릭 시 해당 업무 게시물로 이동.
+    const handleEventClick = (info: EventClickArg) => {
         info.jsEvent.preventDefault(); // don't let the browser navigate
 
         if (info.event.url) {
             window.open(info.event.url);
         }
-    }
+    };
+
+    const closeModal = () => {
+        setModalIsOpen(false);
+    };
 
     return (
-        <FullCalendar
-            plugins={[dayGridPlugin, interactionPlugin, timeGridPlugin, listPlugin]}
-            initialView='dayGridMonth'
-            dayMaxEventRows={true}
-            eventSources={calenderItemList}
-            eventClick={handleEventClick}
-            locale='ko'
-        />
+        <>
+            <FullCalendar
+                plugins={[dayGridPlugin, interactionPlugin, timeGridPlugin, listPlugin]}
+                timeZone={'local'}
+                initialView='dayGridMonth'
+                headerToolbar={{ left: "prevYear,prev,next,nextYear today", center: "title", right: "dayGridMonth,timeGridWeek,timeGridDay" }}
+                dayMaxEventRows={true}
+                selectable={true}
+                select={handleSelectDate}
+                eventSources={calenderItemList}
+                eventClick={handleEventClick}
+                locale='ko'
+            />
+            <Modal
+                isOpen={modalIsOpen}
+                onRequestClose={closeModal}
+                contentLabel="Selected Date Modal"
+                style={{
+                    overlay: {
+                        position: 'fixed',
+                        top: 0,
+                        left: 0,
+                        right: 0,
+                        bottom: 0,
+                        backgroundColor: 'rgba(255, 255, 255, 0.75)',
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        zIndex: 9999
+                    },
+                    content: {
+                        position: 'relative',
+                        width: '600px',
+                        height: '900px',
+                        overflowY: 'auto',  // 수직 스크롤을 활성화
+                        margin: 'auto',
+                        display: 'flex',
+                        flexDirection: 'column',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        zIndex: 10000
+                    }
+                }}
+            >
+                <form id={'calender-modal-form'} onSubmit={handleModalSubmit}>
+                    <div className={'divider'}></div>
+                    <h1>새로운 일정 등록</h1>
+                    <div className={'calender-modal-form-wrapper'}>
+                        <CalendarMiniBoard />
+                        <div className='divider'/>
+                    </div>
+                    <input title={'시작일'} type={'date'} value={selectedDate.start}/>
+                    <input title={'종료일'} type={'datetime-local'} value={selectedDate.end}/>
+                    <div className={'calender-modal-form-button-box'}>
+                        <button className={'calender-modal-form-button-submit'} form={'calender-modal-form'} type="submit">{'등록'}</button>
+                        <button className={'calender-modal-form-button-cancel'} form={'calender-modal-form'} type="button" onClick={closeModal}>{'취소'}</button>
+                    </div>
+                </form>
+            </Modal>
+        </>
     );
-}
+};
