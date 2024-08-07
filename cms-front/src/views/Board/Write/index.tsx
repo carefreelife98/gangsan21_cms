5b9fa614b24547ca579be9aa072da376ef914ffc
CarefreeLife <csm4903@naver.com>
2024-08-07 20:48:11 +0900

    24.08.07 게시물 작성 페이지: startDt, endDt 입력 추가

--- cms-front/src/views/Board/Write/index.tsx
+++ cms-front/src/views/Board/Write/index.tsx
@@ -1,9 +1,10 @@
 import './style.css';
 import {ChangeEvent, useEffect, useRef, useState} from "react";
-import {useBoardStore, useLoginUserStore} from "../../../stores";
+import {useBoardStore} from "../../../stores";
 import {useNavigate} from "react-router-dom";
 import {MAIN_PATH} from "../../../constants";
 import {useCookies} from "react-cookie";
+import dayjs from "dayjs";
 
 //          component: 게시물 작성 화면 컴포넌트          //
 export default function BoardWrite() {
@@ -13,12 +14,20 @@ export default function BoardWrite() {
     // state: 본문 영역 요소 참조 상태
     const contentRef = useRef<HTMLTextAreaElement | null>(null);
 
+    // state: 업무 시작일 설정 요소 참조 상태
+    const startDtRef = useRef<HTMLInputElement | null>(null);
+
+    // state: 업무 종료일 설정 요소 참조 상태
+    const endDtRef = useRef<HTMLInputElement | null>(null);
+
     // state: 이미지 입력 요소 참조 상태
     const imageInputRef = useRef<HTMLInputElement | null>(null);
 
     // state: 게시물 상태 (전역: 페이지 상단 헤더의 '로그인' 버튼에서 하단 내용의 상태를 알아야 함.)
     const {title, setTitle} = useBoardStore();
     const {content, setContent} = useBoardStore();
+    const {startDt, setStartDt} = useBoardStore();
+    const {endDt, setEndDt} = useBoardStore();
     const {boardImageFileList, setBoardImageFileList} = useBoardStore();
     const {resetBoard} = useBoardStore();
 
@@ -47,6 +56,20 @@ export default function BoardWrite() {
         if(!contentRef.current) return;
         contentRef.current.style.height = 'auto';
         contentRef.current.style.height = `${contentRef.current.scrollHeight}px`;
+    };
+
+    // event handler: 업무 시작일 설정 변경 이벤트 처리
+    const onStartDtChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+        const {value} = event.target;
+        setStartDt(value);
+        if(!startDtRef.current) return;
+    };
+
+    // event handler: 업무 종료일 설정 변경 이벤트 처리
+    const onEndDtChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+        const {value} = event.target;
+        setEndDt(value);
+        if(!endDtRef.current) return;
     };
 
     // event handler: 이미지 변경 이벤트 처리
@@ -103,13 +126,32 @@ export default function BoardWrite() {
             return;
         }
         resetBoard();
+        setEndDt(dayjs().format('YYYY-MM-DD') + "T23:59")
     }, []);
 
     //          render: 게시물 작성 화면 렌더정          //
     return (
         <div id='board-write-wrapper'>
             <div className='board-write-container'>
+                <h2 className='board-write-main-title'>{'업무 등록'}</h2>
                 <div className='board-write-box'>
+                    <div className='board-write-date-box'>
+                        <div className='board-write-date-desc'>{'업무 시작일: '}</div>
+                        <input ref={startDtRef}
+                               className='board-write-date'
+                               type={'datetime-local'}
+                               value={startDt}
+                               onChange={onStartDtChangeHandler}
+                        />
+                        <div className='board-write-date-desc'>{'업무 종료일: '}</div>
+                        <input ref={endDtRef}
+                               className='board-write-date'
+                               type={'datetime-local'}
+                               value={endDt}
+                               onChange={onEndDtChangeHandler}
+                        />
+                    </div>
+                    <div className='divider'></div>
                     <div className='board-write-title-box'>
                         <textarea ref={titleRef}
                                   className='board-write-title-textarea'
@@ -143,6 +185,7 @@ export default function BoardWrite() {
                         {imageUrls.map((imageUrl, index) =>
                             <div className='board-write-image-box'>
                                 <img className='board-write-image'
+                                     alt={'업무 게시물 이미지'}
                                      src={imageUrl}/>
                                 <div className='icon-button image-close' onClick={() => onImageCloseButtonClickHandler(index)}>
                                     <div className='icon close-icon'></div>
