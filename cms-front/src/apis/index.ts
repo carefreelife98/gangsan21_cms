8eba3be8b7c4d2c51728ea4446a22c0a393316f7
CarefreeLife <csm4903@naver.com>
2024-08-30 21:05:18 +0900

    24.08.30 EC2 DOMAIN 으로 변경

--- cms-front/src/apis/index.ts
+++ cms-front/src/apis/index.ts
@@ -31,11 +31,10 @@ import {GetSettingResponseDto} from "./response/setting";
 import {PatchSettingRequestDto} from "./request/setting";
 import PatchSettingResponseDto from "./response/setting/patch-setting.response.dto";
 
-const DOMAIN = 'http://localhost:4000';
-// const DOMAIN = `http://43.201.51.14:4000;
+// const DOMAIN = 'http://localhost:4000';
+const DOMAIN = "http://43.201.51.14:4000";
 
 const API_DOMAIN = `${DOMAIN}/api/v1`;
-// const API_DOMAIN = `${DOMAIN}/api/v1`; // 운영 환경 API
 
 const authorization = (accessToken: string) => {
     return { headers: {Authorization: `Bearer ${accessToken}`} };
